a53c4cccfef1b8f3ab39b63cecbe0c11e3feb64a
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-06-01 19:55:21 +0300

    Comment out not working code on server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express')
 
 const app = express()
 
+// app.get('./', (req, res) => res.send('API running')) <-- Not woring for some reason
+
 app.get('/', function(req, res){
   res.send('Application is running')
 })
 
-// app.get('./', (req, res) => res.send('API running'))
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000 // run on port 5000
 
 app.listen(PORT, ( req, res ) => console.log(`Server is running at port ${ PORT }`))

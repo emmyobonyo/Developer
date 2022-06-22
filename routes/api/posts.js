c794df43e826fb17db961e0fdab4887a5823bfcc
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-06-22 17:00:41 +0300

    Post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,23 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth')
+const Post = require('../../models/Posts')
 
 // @route   GET api/posts
 // @desc    Test Route
 // @access  Public
 
-router.get('/', (req, res) => res.send('Posts ...'))
+router.get('/', [ auth, [
+  check('text', 'Text is required').not().isEmpty()
+] ], async(req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return response.status(400).json({ errors: errors.array() })
+  }
+
+  const user = await User 
+})
 
 module.exports = router
